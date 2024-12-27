28884d70bfec300b0e9831141bced5276129ddaa
adityapai05 <adityapai.main@gmail.com>
2024-12-27 20:31:06 +0530

    hamburger menu fixed

--- recipe-blog-landing-page/script.js
+++ recipe-blog-landing-page/script.js
@@ -1,4 +1,28 @@
+
+
+
 window.onload = function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
+
+  document.addEventListener('click', ({ target }) => {
+      if (!menuToggle.contains(target) && !navLinks.contains(target)) {
+          navLinks.classList.remove('active');
+      }
+  });
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', (e) => {
+          e.preventDefault();
+          const target = document.querySelector(anchor.getAttribute('href'));
+          target?.scrollIntoView({ behavior: 'smooth' });
+          navLinks.classList.remove('active');
+      });
+  });
   let currentImageIndex = 0;
   const images = [
     'assets/sangeetabg1.png',
